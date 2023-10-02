a1f6081cb25168c0b1b52522e0953d603c049f77
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-02 18:04:50 -0300

    feature(DartChallenge): Removing xcode default template and adding git ignore

---
+++ DartChallenge/DartChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartChallengeApp.swift
+//  DartChallenge
+//
+//  Created by thaxz on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DartChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
