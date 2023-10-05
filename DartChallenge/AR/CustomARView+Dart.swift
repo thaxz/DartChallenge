e693f7eb9eeeed561ebb8f205ef7730396b5d1df
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-05 10:07:08 -0300

    feature(TrowDart): Trying to trow ball at the center of the screen

--- DartChallenge/AR/CustomARView+Dart.swift
+++ DartChallenge/AR/CustomARView+Dart.swift
@@ -17,9 +17,28 @@ extension CustomARView {
         //physics
         physicalSphere.physicsBody = PhysicsBodyComponent(massProperties: PhysicsMassProperties(shape: .generateSphere(radius: 0.4), mass: 10.0), material: .generate(),mode: .dynamic)
         physicalSphere.generateCollisionShapes(recursive: false)
-        let throwForce = SIMD3<Float>(0, 0, -20)
+        
+        var throwDirection: SIMD3<Float> = [0, 0, -1]
+        switch position {
+        case let p where p.x < -0.5:
+            throwDirection = [-1, 0, 0]
+        case let p where p.x > 0.5:
+            throwDirection = [1, 0, 0]
+        case let p where p.z < -0.5:
+            throwDirection = [0, 0, -1]
+        case let p where p.z > 0.5:
+            throwDirection = [0, 0, 1]
+        default:
+            break
+        }
+        
+        throwDirection = simd_normalize(throwDirection)
+        let throwForce = throwDirection * 20
+        
         let motionComponent = PhysicsMotionComponent(linearVelocity: throwForce, angularVelocity: [0, 0, 0])
         physicalSphere.components.set(motionComponent)
+        
+        
         //add
         ballWorldAnchor.addChild(physicalSphere)
         self.scene.anchors.append(ballWorldAnchor)
@@ -28,7 +47,7 @@ extension CustomARView {
     func reloadBall(at position: SIMD3<Float>){
         scene.removeAnchor(ballWorldAnchor)
         ballEntity = ModelEntity(mesh: .generateSphere(radius: 0.2),
-                               materials: [SimpleMaterial(color: .white, isMetallic: false)])
+                                 materials: [SimpleMaterial(color: .white, isMetallic: false)])
         ballWorldAnchor = AnchorEntity()
         loadBall(physicalSphere: ballEntity, at: position)
     }
@@ -38,7 +57,7 @@ extension CustomARView {
         if (self.ballEntity.transform.translation.y) < -2.0 {
             scene.removeAnchor(ballWorldAnchor)
             ballEntity = ModelEntity(mesh: .generateSphere(radius: 0.2),
-                                   materials: [SimpleMaterial(color: .white, isMetallic: false)])
+                                     materials: [SimpleMaterial(color: .white, isMetallic: false)])
             ballWorldAnchor = AnchorEntity()
         }
     }
