7f362a9edd05ef393a07c19ff8ed248274cf435a
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-05 10:42:42 -0300

    feature(Collisions): Adding todo on ranges

--- DartChallenge/AR/CustomARView+Dart.swift
+++ DartChallenge/AR/CustomARView+Dart.swift
@@ -13,19 +13,36 @@ import RealityKit
 extension CustomARView {
     
     func loadBall(physicalSphere: ModelEntity, at position: SIMD3<Float>) {
-        ballWorldAnchor.position = position
-        //physics
+            ballWorldAnchor.position = position
+            //physics
         physicalSphere.physicsBody = PhysicsBodyComponent(massProperties: PhysicsMassProperties(shape: .generateBox(width: 1, height: 1, depth: 2), mass: 10.0), material: .generate(),mode: .dynamic)
-        physicalSphere.generateCollisionShapes(recursive: false)
-        
-        let throwForce = SIMD3<Float>(0, 5, -40)
-        let motionComponent = PhysicsMotionComponent(linearVelocity: throwForce, angularVelocity: [0, 0, 0])
-        physicalSphere.components.set(motionComponent)
-        
-        //add
-        ballWorldAnchor.addChild(physicalSphere)
-        self.scene.anchors.append(ballWorldAnchor)
-    }
+            physicalSphere.generateCollisionShapes(recursive: false)
+            
+        //todo: make more ranges
+            var throwDirection: SIMD3<Float> = [0, 0, -40]
+            switch position {
+            case let p where p.x < -0.5:
+                throwDirection = [-1, 0, 0]
+            case let p where p.x > 0.5:
+                throwDirection = [1, 0, 0]
+            case let p where p.z < -0.5:
+                throwDirection = [0, 0, -1]
+            case let p where p.z > 0.5:
+                throwDirection = [0, 0, 1]
+            default:
+                break
+            }
+            
+            throwDirection = simd_normalize(throwDirection)
+            let throwForce = throwDirection * 40
+            
+            let motionComponent = PhysicsMotionComponent(linearVelocity: throwForce, angularVelocity: [0, 0, 0])
+            physicalSphere.components.set(motionComponent)
+            
+            //add
+            ballWorldAnchor.addChild(physicalSphere)
+            self.scene.anchors.append(ballWorldAnchor)
+        }
     
     
     func updateDart(){
