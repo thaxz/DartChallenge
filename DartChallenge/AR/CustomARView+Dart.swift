8c5f60140df5064bf7318d269c63f5580a0a26b6
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-05 08:57:42 -0300

    feature(Design): Ball is now always being thrown at the center of the screen

--- DartChallenge/AR/CustomARView+Dart.swift
+++ DartChallenge/AR/CustomARView+Dart.swift
@@ -14,22 +14,32 @@ extension CustomARView {
     
     func loadBall(physicalSphere: ModelEntity, at position: SIMD3<Float>) {
         ballWorldAnchor.position = position
-        createDartPhysics(physicalSphere: physicalSphere)
-        ballWorldAnchor.addChild(physicalSphere)
-        self.scene.anchors.append(ballWorldAnchor)
-    }
-    
-    func createDartPhysics(physicalSphere: ModelEntity){
+        //physics
         physicalSphere.physicsBody = PhysicsBodyComponent(massProperties: PhysicsMassProperties(shape: .generateSphere(radius: 0.4), mass: 10.0), material: .generate(),mode: .dynamic)
         physicalSphere.generateCollisionShapes(recursive: false)
         let throwForce = SIMD3<Float>(0, 0, -20)
         let motionComponent = PhysicsMotionComponent(linearVelocity: throwForce, angularVelocity: [0, 0, 0])
         physicalSphere.components.set(motionComponent)
+        //add
+        ballWorldAnchor.addChild(physicalSphere)
+        self.scene.anchors.append(ballWorldAnchor)
     }
+    
+    func reloadBall(at position: SIMD3<Float>){
+        scene.removeAnchor(ballWorldAnchor)
+        ballEntity = ModelEntity(mesh: .generateSphere(radius: 0.2),
+                               materials: [SimpleMaterial(color: .white, isMetallic: false)])
+        ballWorldAnchor = AnchorEntity()
+        loadBall(physicalSphere: ballEntity, at: position)
+    }
+    
     
     func updateDart(){
         if (self.ballEntity.transform.translation.y) < -2.0 {
             scene.removeAnchor(ballWorldAnchor)
+            ballEntity = ModelEntity(mesh: .generateSphere(radius: 0.2),
+                                   materials: [SimpleMaterial(color: .white, isMetallic: false)])
+            ballWorldAnchor = AnchorEntity()
         }
     }
     
