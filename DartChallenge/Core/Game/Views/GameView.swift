c2a4b6d6f0e1c8b0f0d6a9c9e842595f039d8464
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-08 15:07:31 -0300

    feature(Design): Upading gameView

--- DartChallenge/Core/Game/Views/GameView.swift
+++ DartChallenge/Core/Game/Views/GameView.swift
@@ -9,45 +9,53 @@ import SwiftUI
 import ARKit
 
 struct GameView: View {
+    
     @StateObject private var coordinator = Coordinator()
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             ARViewContainer(coordinator: coordinator)
                 .ignoresSafeArea()
             VStack {
+                pauseSection
                 Spacer()
-                HStack {
-                    Button {
-                        ARManager.shared.actionsStream.send(.placeBoard)
-                    } label: {
-                        Image(systemName: "circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                            .padding()
-                            .background(.regularMaterial)
-                            .cornerRadius(16)
-                    }
-                    Button {
+                    PrimaryButton(title: "throw") {
                         coordinator.placeDart()
-                    } label: {
-                        Image(systemName: "square.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                            .padding()
-                            .background(.regularMaterial)
-                            .cornerRadius(16)
                     }
-                }
+                    .padding(.horizontal, 30)
+                    PrimaryButton(title: "place board") {
+                        ARManager.shared.actionsStream.send(.placeBoard)
+                    }
+                    .padding(.horizontal, 30)
             }
+            .padding(.horizontal, 20)
             .onReceive(timer) { _ in
                 ARManager.shared.actionsStream.send(.removeDart)
 //                ARManager.shared.actionsStream.send(.checkCollision)
             }
         }
     }
+}
+
+// MARK: Components
+
+extension GameView {
+    
+    var pauseSection: some View{
+        HStack{
+            Spacer()
+            Button {
+                // pause
+            } label: {
+                Image(systemName: "pause.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
