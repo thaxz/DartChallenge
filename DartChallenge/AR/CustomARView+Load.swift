21f689e9ffc623062fed95b87add72c69d6a18cb
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-08 14:41:10 -0300

    feature(Design): Starting primary button

--- DartChallenge/AR/CustomARView+Load.swift
+++ DartChallenge/AR/CustomARView+Load.swift
@@ -16,14 +16,14 @@ extension CustomARView {
         self.scene.anchors.append(worldAnchor)
     }
     
-    func oldPlaceDart(){
-        let anchor = AnchorEntity(plane: .horizontal)
-        guard let dartEntity = try? Entity.load(named: "firstDart") else {return}
-        dartEntity.addPhysicsToChildren()
-        anchor.addChild(dartEntity)
-        scene.addAnchor(anchor)
-        
-    }
+//    func oldPlaceDart(){
+//        let anchor = AnchorEntity(plane: .horizontal)
+//        guard let dartEntity = try? Entity.load(named: "firstDart") else {return}
+//        dartEntity.addPhysicsToChildren()
+//        anchor.addChild(dartEntity)
+//        scene.addAnchor(anchor)
+//        
+//    }
     
 }
 
--- DartChallenge/Core/Components/PrimaryButton.swift
+++ DartChallenge/Core/Components/PrimaryButton.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct PrimaryButton: View {
+    let title: String
+    let action: () -> ()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(Color.theme.primary)
+                Text(title.uppercased())
+                    .foregroundColor(.white)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 60)
     }
 }
 
 struct PrimaryButton_Previews: PreviewProvider {
     static var previews: some View {
-        PrimaryButton()
+        PrimaryButton(title: "play", action: {})
     }
 }
