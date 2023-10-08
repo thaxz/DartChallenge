f9a6bfe2eb7731aaf83e7f686c054a05f8ae3e9b
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-08 14:35:54 -0300

    feature(Design): Adding assets and color scheme

---
+++ DartChallenge/Core/Components/PrimaryButton.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryButton.swift
+//  DartChallenge
+//
+//  Created by thaxz on 08/10/23.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton()
+    }
+}
